b52329f891b9e4289991179e6986350652bfac40
Aaron <qikunlele@outlook.com>
2025-03-15 21:06:53 +0800

    Feat: 增加对 prometheus 的支持，多环境打包的支持

---
+++ web/src/main/java/com/interview/web/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.interview.web.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author qikun
+ * @date 3/15/2025  7:43 PM
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 如果是其他数据库，替换成对应的DbType
+        return interceptor;
+    }
+}
--- web/src/main/java/com/interview/web/controller/StudentController.java
+++ web/src/main/java/com/interview/web/controller/StudentController.java
@@ -8,6 +8,8 @@ import com.interview.web.pojo.entity.Student;
 import com.interview.web.pojo.param.StudentParam;
 import com.interview.web.repository.ArticleDocRepository;
 import com.interview.web.service.StudentService;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +17,11 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.Random;
 /**
  * <p>
  *  前端控制器
@@ -31,6 +35,16 @@ import java.util.List;
 @RequestMapping("/interview/student")
 @Slf4j
 public class StudentController {
+
+    private final Counter requestCounter;
+    private final MeterRegistry meterRegistry;
+    private final Random random = new Random();
+
+    @Autowired
+    public StudentController(Counter requestCounter, MeterRegistry meterRegistry) {
+        this.requestCounter = requestCounter;
+        this.meterRegistry = meterRegistry;
+    }
     @Autowired
     private StudentService studentService;
 
@@ -42,6 +56,9 @@ public class StudentController {
 
     @PostMapping("/add")
     private ApiResultI18n add(@RequestBody @Valid StudentParam studentParam) {
+        // 模拟错误计数
+        meterRegistry.counter("app.errors.count").increment();
+
         log.info(greetingService.greet("student add"));
         Student student = Convert.convert(Student.class, studentParam);
         return ApiResultI18n.success(studentService.save(student));
@@ -49,8 +66,35 @@ public class StudentController {
 
     @GetMapping("/list")
     private ApiResultI18n<List<Student>> list() {
+        // 增加请求计数器
+        requestCounter.increment();
+        AtomicReference<List<Student>> list = new AtomicReference<>(new ArrayList<>());
+        // 记录响应时间 (模拟)
+        meterRegistry.timer("app.hello.timer").record(() -> {
+            try {
+                Thread.sleep(random.nextInt(200) + 100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list.set(studentService.list());
+        });
         log.info(greetingService.greet("student list"));
-        return ApiResultI18n.success(studentService.list());
+        return ApiResultI18n.success(list);
+    }
+
+    @GetMapping("/bye")
+    private ApiResultI18n hello() {
+        // 增加请求计数器
+        requestCounter.increment();
+        // 记录响应时间 (模拟)
+        meterRegistry.timer("app.bye.timer").record(() -> {
+            try {
+                Thread.sleep(random.nextInt(100) + 1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return ApiResultI18n.success(true);
     }
 
     @GetMapping("/test")
